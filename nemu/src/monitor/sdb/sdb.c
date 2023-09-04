3abb1862b854295a37d59fd3627f33c9a191466b
tracer-ysyx <tracer@ysyx.org>
2023-09-04 22:28:58 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:28:58 up  1:19,  4 users,  load average: 1.03, 0.76, 0.62

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -70,6 +70,11 @@ static int cmd_si(char *args) {
 }
 
 static int cmd_info(char *args) {
+  if(args==NULL) {
+    printf("default display all info\n");
+    return 0;
+  }
+
   if(strcmp(args,"r")){
     isa_reg_display();
   }
