94900ce76855482c344fcd39c5f3a3b68a7f88cc
tracer-ysyx <tracer@ysyx.org>
2023-09-08 20:37:53 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:37:53 up 11:34,  1 user,  load average: 0.53, 0.40, 0.46

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -21,6 +21,7 @@
 #include <string.h>
 
 // this should be enough
+static char is_too_long = 0;
 static char buf[65536] = "";
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
@@ -31,22 +32,39 @@ static char *code_format =
     "  return 0; "
     "}";
 static int buf_p = 0;
+uint32_t gen_rand_expr_with_ret();
 void gen(char c)
 {
   buf[buf_p++] = c;
   buf[buf_p] = '\0';
 }
+int choose(int n)
+{
+  return rand() % (n);
+}
 uint32_t gen_num()
 {
+  if ((65535 - buf_p) < 11)
+  {
+    is_too_long = 1;
+    return 1;
+  }
   uint32_t temp = rand() & 0xffffffff;
   char num[65536];
   sprintf(num, "%u", temp);
   memcpy(buf + buf_p, num, strlen(num));
   buf_p += strlen(num);
+  buf[buf_p] = '\0';
   return temp;
 }
+
 int gen_rand_op()
 {
+  if ((65535 - buf_p) < 11)
+  {
+    is_too_long = 1;
+    return 1;
+  }
   int t = rand() & 3;
   char op = '\0';
   switch (t)
@@ -68,12 +86,49 @@ int gen_rand_op()
   buf[buf_p] = '\0';
   return t;
 }
-int choose(int n)
+static void gen_rand_expr()
 {
-  return rand() % (n);
+  if ((65535 - buf_p) < 11)
+  {
+    is_too_long = 1;
+  }
+  switch (choose(3))
+  {
+  case 0:
+    gen_num();
+    break;
+  case 1:
+    gen('(');
+    gen_rand_expr();
+    gen(')');
+    break;
+  default:
+    gen_rand_expr();
+    int op = gen_rand_op();
+    if (op == 3)
+    {
+      int now = buf_p;
+      uint32_t div = gen_rand_expr_with_ret();
+      while (div == 0)
+      {
+        buf_p = now;
+        div = gen_rand_expr_with_ret();
+      }
+    }
+    else
+    {
+      gen_rand_expr();
+    }
+  }
 }
-static uint32_t gen_rand_expr()
+
+uint32_t gen_rand_expr_with_ret()
 {
+  if ((65535 - buf_p) < 11)
+  {
+    is_too_long = 1;
+    return 1;
+  }
   uint32_t res = 0;
   switch (choose(3))
   {
@@ -82,40 +137,44 @@ static uint32_t gen_rand_expr()
     return res;
   case 1:
     gen('(');
-    res = gen_rand_expr();
+    res = gen_rand_expr_with_ret();
     gen(')');
     return res;
+    break;
   default:
-    uint32_t dividend = gen_rand_expr();
+    uint32_t first = gen_rand_expr_with_ret(), second = 0;
     int op = gen_rand_op();
-    uint32_t divisor = gen_rand_expr();
     if (op == 3)
     {
-      if (divisor == 0)
+      int now = buf_p;
+      while (second == 0)
       {
-        printf("%u %d %u ,divisor can not be zero \n", dividend, op, divisor);
-        assert(0);
+        buf_p = now;
+        second = gen_rand_expr_with_ret();
       }
+    }
+    else
+    {
+      second = gen_rand_expr_with_ret();
     }
     switch (op)
     {
     case 0:
-      res = dividend + divisor;
+      res = first + second;
       break;
     case 1:
-      res = dividend - divisor;
+      res = first - second;
       break;
     case 2:
-      res = dividend * divisor;
+      res = first * second;
       break;
     case 3:
-      res = dividend / divisor;
+      res = first / second;
       break;
     }
-    return res;
   }
+  return res;
 }
-
 int main(int argc, char *argv[])
 {
   int seed = time(0);
@@ -130,7 +189,12 @@ int main(int argc, char *argv[])
   {
     buf_p = 0;
     gen_rand_expr();
-
+    if (is_too_long == 1)
+    {
+      is_too_long = 0;
+      i--;
+      continue;
+    }
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
