e68196c4c4661dcca4a05a9f196f452cd094dec6
tracer-ysyx <tracer@ysyx.org>
2023-09-09 21:06:28 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:06:28 up 11:58,  1 user,  load average: 0.28, 0.20, 0.19

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -44,7 +44,6 @@ int main(int argc, char *argv[])
   while (fgets(buf, 65536 + 128, fp) != NULL)
   {
     sscanf(buf, "%u %s", &ans, expression);
-    printf("expression: %s ", expression);
     eval = expr(expression, &success);
     if (success)
     {
