2b739ef700a910b4b90af245a4a5c115dc3f3c6d
tracer-ysyx <tracer@ysyx.org>
2023-09-10 09:39:48 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:39:48 up  1:10,  1 user,  load average: 0.28, 0.34, 0.35

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -30,6 +30,7 @@ int main(int argc, char *argv[])
   init_monitor(argc, argv);
 #endif
   // test the expression
+  /*
   char buf[65536 + 128];
   uint32_t ans = 0, eval = 0;
   char expression[65536] = "";
@@ -58,6 +59,7 @@ int main(int argc, char *argv[])
       }
     }
   }
+  */
 
   /* Start engine. */
   engine_start();
