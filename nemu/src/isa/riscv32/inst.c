4b367a092f76e26d00a5dcce994eccce26959761
tracer-ysyx <tracer@ysyx.org>
2023-09-21 00:12:50 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:12:50 up 16:01,  5 users,  load average: 0.24, 0.29, 0.27

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -13,6 +13,7 @@
  * See the Mulan PSL v2 for more details.
  ***************************************************************************************/
 
+#include "isa.h"
 #include "local-include/reg.h"
 #include "macro.h"
 #include <cpu/cpu.h>
@@ -274,7 +275,7 @@ static int decode_exec(Decode *s)
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
-
+  isa_reg_display();
   return 0;
 }
 
