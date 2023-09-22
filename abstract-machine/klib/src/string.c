f23a50430dac9550cf6f33edab487106245db552
tracer-ysyx <tracer@ysyx.org>
2023-09-22 12:02:03 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:02:03 up  3:16,  1 user,  load average: 1.21, 0.80, 0.72

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,27 +5,61 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t i;
+  for(i = 0;*s!='\0';i++);
+  return i;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *temp = dst;
+  while(*src!='\0'){
+    *(temp++) = *(src++);
+  }
+  *temp = '\0';
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i = 0;
+  for(;i<n&&src[i]!='\0';i++){
+    dst[i] = src[i];
+  }
+  dst[i] = '\0';
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *p = dst;
+  while(*p!='\0'){
+    p++;
+  }
+  while(*src!='\0'){
+    *(p++) = *(src++);
+  }
+  *p = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  while(*s1!='\0'&&*s2!='\0'){
+    if(*s1==*s2) continue;
+    return *s1-*s2;
+  }
+  if(*s1=='\0'&&*s2=='\0')
+    return 0;
+  if(*s1=='\0') return -1;
+  return 1;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t i = 0;
+  for(;i<n&&s1[i]!='\0'&&*s2!='\0';i++){
+    if(s1[i]==s2[i]) continue;
+    return s1[i]-s2[i];
+  }
+  if(i==n||(s1[i]=='\0'&&s2[i]=='\0')) return 0;
+  if(s1[i]=='\0') return -1;
+  return 1;
 }
 
 void *memset(void *s, int c, size_t n) {
