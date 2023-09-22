69403b4aeada3ff51bdd9ef37d7f2197913ff42e
tracer-ysyx <tracer@ysyx.org>
2023-09-22 15:43:04 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:43:04 up  6:57,  3 users,  load average: 0.46, 0.47, 0.43

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+#define INT2STR(x) #x
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +14,49 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  //todo
+  /*
+  实现abstract-machine/klib/src/stdio.c中的sprintf(),
+  具体行为可以参考man 3 printf. 目前你只需要实现%s和%d就能通过hello-str的测试了, 
+  其它功能(包括位宽, 精度等)可以在将来需要的时候再自行实现.
+  */
+  va_list ap;
+  va_start(ap,fmt);                                                                                                                                       
+  int num = 0;
+  int op = 0;
+  for(int i=0;i<=strlen(fmt);i++){
+      if(fmt[i]!='%') {
+        out[op] = fmt[i];
+        op++;
+        continue;
+      }
+      if(fmt[i+1] == 'd'){
+          int t = va_arg(ap,int);
+          char *p = INT2STR(t);
+          printf("%d\n",t);
+          i += 2;
+          num ++;
+          int len = strlen(p);
+          for(int j = 0;j<len;j++){
+            out[op++]=p[j];
+          }
+          continue;
+      }
+      if(fmt[i+1] == 's'){
+          char *p = va_arg(ap,char *);
+          printf("%s\n",p);
+          i += 2;
+          num ++;
+          int len = strlen(p);
+          for(int j=0;j<len;j++){
+            out[op++] = p[j];
+          }
+          continue;
+      }
+  }
+  va_end(ap);
+  return num;
+
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
