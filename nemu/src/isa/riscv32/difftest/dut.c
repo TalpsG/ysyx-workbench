1972555075c1acbbdc71940899bd277dc00fc685
tracer-ysyx <tracer@ysyx.org>
2023-09-24 09:54:59 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:54:59 up  1:09,  1 user,  load average: 0.64, 0.72, 0.69

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -15,10 +15,23 @@
 
 #include <isa.h>
 #include <cpu/difftest.h>
+#include <stdio.h>
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  if(pc!=ref_r->pc){
+    printf("pc is diff\n");
+    printf("ref : 0x%08x\n",ref_r->pc);
+    printf("nemu: 0x%08x\n",pc);
+  }
+  for(int i=0;i<32;i++){
+    if(ref_r->gpr[i]!=cpu.gpr[i]) {
+      printf("reg is diff\n");
+      return false;
+    }
+  }
+
+  return true;
 }
 
 void isa_difftest_attach() {
