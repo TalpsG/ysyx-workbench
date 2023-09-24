59c417b5a1243c1f2c91553f248cd1dd3a5c6026
tracer-ysyx <tracer@ysyx.org>
2023-09-24 10:56:22 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:56:22 up  2:10,  1 user,  load average: 0.78, 0.65, 0.54

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -41,7 +41,7 @@ static bool g_print_step = false;
 ringbuffer for log
 */
 #define BUF_SIZE 200
-#define LINE_SIZE 200
+#define LINE_SIZE 500
 char ringbuf[BUF_SIZE][LINE_SIZE];
 int pos=0;
 void check_call(Decode s){
@@ -96,10 +96,10 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
     if(nemu_state.state == NEMU_ABORT||
       (nemu_state.state == NEMU_END &&
        nemu_state.halt_ret == 1)){
-      sprintf(ringbuf[pos] , " ---> %100s\n",_this->logbuf) ;
+      sprintf(ringbuf[pos] , " ---> %400s\n",_this->logbuf) ;
       flush_ringbuf();
     }else{
-      sprintf(ringbuf[pos] , "      %100s\n",_this->logbuf) ;
+      sprintf(ringbuf[pos] , "      %400s\n",_this->logbuf) ;
       pos = (pos+1)%BUF_SIZE;
     }
   }
