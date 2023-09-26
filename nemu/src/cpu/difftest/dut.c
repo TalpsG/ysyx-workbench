a8722d46bb3fb64aab406c8097650d500ae744cf
tracer-ysyx <tracer@ysyx.org>
2023-09-26 15:22:01 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:22:01 up  6:39,  1 user,  load average: 0.37, 0.78, 0.88

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -18,6 +18,7 @@
 #include <isa.h>
 #include <cpu/cpu.h>
 #include <memory/paddr.h>
+#include <stdio.h>
 #include <utils.h>
 #include <difftest-def.h>
 
@@ -92,12 +93,13 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 }
 
 static void checkregs(CPU_state *ref, vaddr_t pc) {
-  if (!isa_difftest_checkregs(ref, pc)) {
-    printf("reg is diff\n");
-    nemu_state.state = NEMU_ABORT;
-    nemu_state.halt_pc = pc;
-    isa_reg_display();
-  }
+	printf("check regs\n");
+	if (!isa_difftest_checkregs(ref, pc)) {
+		printf("reg is diff\n");
+		nemu_state.state = NEMU_ABORT;
+		nemu_state.halt_pc = pc;
+		isa_reg_display();
+	}
 }
 
 void difftest_step(vaddr_t pc, vaddr_t npc) {
