2a0380efa165bcb26b98f6160ad45481d8ced5f4
tracer-ysyx <tracer@ysyx.org>
2023-09-26 15:30:11 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:30:11 up  6:47,  1 user,  load average: 0.56, 0.67, 0.78

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -25,64 +25,64 @@
 #include "../local-include/reg.h"
 #include "cpu/difftest.h"
 
-__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  size_t num = n;
-  if(direction==DIFFTEST_TO_REF){
-    while(num>0){
-      if(num>=4){
-        paddr_write(addr, 4,*(uint32_t*) buf);
-        num -= 4;
-        buf+=4;
-      }else if (num>=2)
-      {
-        paddr_write(addr, 2,*(uint16_t*) buf);
-        num -= 2;
-        buf+=2;
-      }else if (num>=1)
-      {
-        paddr_write(addr, 1,*(uint8_t*) buf);
-        num -= 1;
-        buf += 1;
-      }
-    }
-  }else{
-    while(num>0){
-      if(num>=4){
-        uint32_t t = paddr_read(addr, 4);
-        *(uint32_t*)buf = t;
-        num -= 4;
-        buf+=4;
-      }else if (num>=2)
-      {
-        uint16_t t = paddr_read(addr, 2);
-        *(uint16_t*)buf = t;
-        num -= 2;
-        buf+=2;
-      }else if (num>=1)
-      {
-        uint8_t t = paddr_read(addr, 1);
-        *(uint8_t*)buf = t;
-        num -= 1;
-        buf += 1;
-      }
-    }
+//__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+  //size_t num = n;
+  //if(direction==DIFFTEST_TO_REF){
+    //while(num>0){
+      //if(num>=4){
+        //paddr_write(addr, 4,*(uint32_t*) buf);
+        //num -= 4;
+        //buf+=4;
+      //}else if (num>=2)
+      //{
+        //paddr_write(addr, 2,*(uint16_t*) buf);
+        //num -= 2;
+        //buf+=2;
+      //}else if (num>=1)
+      //{
+        //paddr_write(addr, 1,*(uint8_t*) buf);
+        //num -= 1;
+        //buf += 1;
+      //}
+    //}
+  //}else{
+    //while(num>0){
+      //if(num>=4){
+        //uint32_t t = paddr_read(addr, 4);
+        //*(uint32_t*)buf = t;
+        //num -= 4;
+        //buf+=4;
+      //}else if (num>=2)
+      //{
+        //uint16_t t = paddr_read(addr, 2);
+        //*(uint16_t*)buf = t;
+        //num -= 2;
+        //buf+=2;
+      //}else if (num>=1)
+      //{
+        //uint8_t t = paddr_read(addr, 1);
+        //*(uint8_t*)buf = t;
+        //num -= 1;
+        //buf += 1;
+      //}
+    //}
 
-  }
-}
+  //}
+//}
 
-__EXPORT void difftest_regcpy(void *dut, bool direction) {
-  CPU_state *p = dut;
-  if(direction == DIFFTEST_TO_REF){
-    for(int i=0;i<32;i++){
-      p->gpr[i] = gpr(i)+1;
-    }
-  }
-  else{
-    for(int i=0;i<32;i++){
-      gpr(i) = p->gpr[i]+1;
-    }
-  }
-}
+//__EXPORT void difftest_regcpy(void *dut, bool direction) {
+  //CPU_state *p = dut;
+  //if(direction == DIFFTEST_TO_REF){
+    //for(int i=0;i<32;i++){
+      //p->gpr[i] = gpr(i)+1;
+    //}
+  //}
+  //else{
+    //for(int i=0;i<32;i++){
+      //gpr(i) = p->gpr[i]+1;
+    //}
+  //}
+//}
 
 __EXPORT void difftest_exec(uint64_t n) {
   assert(0);
