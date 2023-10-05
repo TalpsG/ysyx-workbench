59add62a2713bb13864adb87a398ac781642a05b
tracer-ysyx <tracer@ysyx.org>
2023-10-05 10:01:12 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:01:12 up  1:35,  1 user,  load average: 0.13, 0.25, 0.29

--- npc/myrtl/csrc/difftest.cpp
+++ npc/myrtl/csrc/difftest.cpp
@@ -1,12 +1,15 @@
 #include "utils.h"
+extern Vtop top;
 struct context {
   uint32_t gpr[32];
   uint32_t pc;
 };
-void 
+void check_regs(void *dut, uint32_t pc) {
+
+}
 void difftest_step(uint32_t pc,uint32_t npc) {
   struct context ref;
   difftest_exec(1);
   difftest_regcpy(&ref,DIFFTEST_TO_DUT );
-  check_regs();
+  check_regs(ref,pc);
 }
