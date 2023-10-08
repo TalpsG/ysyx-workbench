23001cd4211ce7119df827645967edd9456f5dbb
tracer-ysyx <tracer@ysyx.org>
2023-10-08 17:36:06 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:36:06 up  9:01,  4 users,  load average: 0.26, 0.25, 0.33

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,7 +4,8 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
-
+static void *mem_sbrk = 0;
+extern Area heap;
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
@@ -34,9 +35,18 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+
+  //panic("Not implemented");
 #endif
-  return NULL;
+static int first = 0;
+	if (first == 0) {
+		first = 1;
+		mem_sbrk = heap.start;
+	} 
+	size = (size+3)&(~0x3u);
+	void *temp = mem_sbrk;
+	mem_sbrk += size;
+  return temp;
 }
 
 void free(void *ptr) {
