6c35174c2a26c451c3f5a2f932fcf50aaeb2195f
tracer-ysyx <tracer@ysyx.org>
2023-10-09 11:27:08 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:27:08 up  1:45,  3 users,  load average: 0.53, 0.55, 0.61

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -42,7 +42,7 @@ static int first = 0;
 	if (first == 0) {
 		first = 1;
 		mem_sbrk = heap.start;
-		printf("heapstart:%d\n",(int)mem_sbrk);
+		printf("heapstart:%d\n",mem_sbrk);
 	} 
 	size = (size+3)&(~0x3u);
 	void *temp = mem_sbrk;
