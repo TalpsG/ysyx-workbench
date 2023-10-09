9b177e6831b3ac558160250fb26b1dfaf8dbc55f
tracer-ysyx <tracer@ysyx.org>
2023-10-09 21:21:20 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:21:20 up 11:39,  4 users,  load average: 0.70, 0.59, 0.59

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -74,7 +74,7 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-	printf("read    --> addr :%8x ,            len:%d\n",addr,len);
+	//printf("read    --> addr :%8x ,            len:%d\n",addr,len);
   if (likely(in_pmem(addr))) return  pmem_read(addr, len);
   else {IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));}
   out_of_bound(addr);
@@ -82,7 +82,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-	printf("write   --> addr :%8x ,data :%8x , len:%d\n",addr,data,len);
+	//printf("write   --> addr :%8x ,data :%8x , len:%d\n",addr,data,len);
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
