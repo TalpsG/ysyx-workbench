39a7f8fea9f4b074a90fba576592e0ae83ec2bdf
tracer-ysyx <tracer@ysyx.org>
2023-10-09 21:04:53 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:04:53 up 11:23,  4 users,  load average: 0.61, 0.69, 0.62

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -76,7 +76,7 @@ void send_key(uint8_t scancode, bool is_keydown) {
     uint32_t am_scancode = keymap[scancode] | (is_keydown ? KEYDOWN_MASK : 0);
 #ifdef CONFIG_DTRACE
   char buf[100];
-	sprintf(buf,"device:%10s read  ----> addr:%8x ,len:%2d data:%8x\n", "keyboard",CONFIG_I8042_DATA_MMIO,4,am_scancode);
+	sprintf(buf,"device:%10s read  ----> addr:%8x ,len:%2d data:%8x\n", "kbd send",CONFIG_I8042_DATA_MMIO,4,am_scancode);
   add_dtrace(buf);
 #endif
     key_enqueue(am_scancode);
@@ -100,7 +100,7 @@ static void i8042_data_io_handler(uint32_t offset, int len, bool is_write) {
   i8042_data_port_base[0] = key_dequeue();
 #ifdef CONFIG_DTRACE
   char buf[100];
-	sprintf(buf,"device:%10s read  ----> addr:%8x ,len:%2d data:%8x\n", "keyboard",CONFIG_I8042_DATA_MMIO,len,i8042_data_port_base[0]);
+	sprintf(buf,"device:%10s read  ----> addr:%8x ,len:%2d data:%8x\n", "kbd handle",CONFIG_I8042_DATA_MMIO,len,i8042_data_port_base[0]);
   add_dtrace(buf);
 #endif
 }
