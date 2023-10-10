46056c0af65fbca810cf8fa24b61e4592c232a9d
tracer-ysyx <tracer@ysyx.org>
2023-10-10 09:09:45 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:09:45 up 53 min,  4 users,  load average: 1.52, 0.81, 0.48

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -15,9 +15,13 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 			kbd->keydown = false;
 		}
 		else if ((make_code & 0x8000)) {
+			putch('d');
+			putch('\n');
 			kbd->keycode = code ;
 			kbd->keydown = true;
 		} else {
+			putch('u');
+			putch('\n');
 			kbd->keydown = AM_KEY_NONE;
 			kbd->keydown = false;
 			wait = 1;
