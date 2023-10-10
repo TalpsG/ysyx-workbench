a8a3acc3daec5be5a0257963bf5f237f9c2b3812
tracer-ysyx <tracer@ysyx.org>
2023-10-10 11:34:51 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:34:51 up  3:18,  4 users,  load average: 0.49, 0.43, 0.40

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -9,7 +9,7 @@ void __am_gpu_init() {
 	int w = ctl>>16;  // TODO: get the correct width
 	int h = ctl&0xffff;  // TODO: get the correct height
 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-	for (i = 0; i < w * h; i ++) fb[i] = i;
+	for (i = 0; i < w * h; i ++) fb[i] = 0;
 	outl(SYNC_ADDR, 1);
 }
 
