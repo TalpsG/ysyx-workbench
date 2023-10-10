70769aabd4423620c05f331454ed1e3870082d08
tracer-ysyx <tracer@ysyx.org>
2023-10-10 13:42:15 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:42:15 up  5:25,  4 users,  load average: 0.89, 0.78, 0.49

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -27,6 +27,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
 	unsigned int vga_w = inl(VGACTL_ADDR)>>16;
 	int w = ctl->w,h = ctl->h;
+	int x = ctl->x,y=ctl->y;
+	printf("x:%x,y:%d,w:%d,h:%d\n",x,y,w,h);
 	for (int i = 0; i < h; i++) {
 		for (int j = 0; j < w; j++) {
 			printf("pos:%d\n", i * vga_w + j);
