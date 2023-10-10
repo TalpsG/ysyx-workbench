cc36742a0eb8b59093fc52371aa512b0325fbe35
tracer-ysyx <tracer@ysyx.org>
2023-10-10 09:26:37 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:26:37 up  1:10,  4 users,  load average: 1.11, 0.91, 0.70

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,10 +4,10 @@
 
 #define KEYDOWN_MASK 0x8000
 
+static int wait = 0;
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 	unsigned int kbd_reg = inl(KBD_ADDR);
 	unsigned int code = kbd_reg &0xff;
-	static int wait = 0;
 	if (code != 0) {
 		printf("keycode:%d\n",kbd_reg);
 		if (wait == 1) {
