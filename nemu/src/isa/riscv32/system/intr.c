6091a611f9b3b74a85fd484df217a157c167025f
tracer-ysyx <tracer@ysyx.org>
2023-10-12 16:56:20 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:56:20 up  7:39,  6 users,  load average: 0.48, 0.57, 0.49

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -14,11 +14,44 @@
 ***************************************************************************************/
 
 #include <isa.h>
-
+static uint32_t mcause=0;
+static uint32_t mtvec=0;
+static uint32_t mepc=0;
+static uint32_t mstatus=0;
+word_t csr_read(word_t index) {
+	switch (index)
+	{
+        case 0:
+			return mstatus;
+        case 1:
+			return mepc;
+        case 2:
+			return mcause;
+        case 5:
+			return mtvec;
+	}
+	return 0;
+}
+word_t csr_write(word_t index,word_t data) {
+	switch (index)
+	{
+        case 0:
+          mstatus = data;break;
+        case 1:
+          mepc= data;break;
+        case 2:
+          mcause = data;break;
+        case 5:
+          mtvec = data;break;
+	}
+	return 0;
+}
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+  mcause = NO;
+  mepc = epc;
 
   return 0;
 }
