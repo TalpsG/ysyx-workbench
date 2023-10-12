9ae109a54cb86b8d56c174068f974a2f1ab25dbe
tracer-ysyx <tracer@ysyx.org>
2023-10-12 22:31:09 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:31:09 up 13:13,  2 users,  load average: 0.31, 0.41, 0.40

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -68,7 +68,7 @@ word_t mmio_read(paddr_t addr, int len) {
 }
 
 void mmio_write(paddr_t addr, int len, word_t data) {
-	char buf[100];
+	char buf[200];
   IOMap* iop =  fetch_mmio_map(addr);
   sprintf(buf,"pc:%8x, device:%10s write <---- addr:%8x ,len:%2d data:%8x\n", cpu.pc,iop->name,addr,len,data);
   add_dtrace(buf);
