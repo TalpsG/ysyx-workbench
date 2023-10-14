f4b1e6e7ed3f3f76eef1b3e6470276be4974d39d
tracer-ysyx <tracer@ysyx.org>
2023-10-14 10:11:32 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:11:32 up  1:17,  3 users,  load average: 0.63, 0.43, 0.31

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -36,13 +36,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 	Context *p = kstack.end - sizeof(Context);
 	p->mepc = (unsigned int)entry;
-	char *c = (char*)&(p->mepc);
-	printf("kontext ");
-	putch(c[0]);
-	putch(c[1]);
-	putch(c[2]);
-	putch(c[3]);
-	putch('\n');
+	printf("kontext %d\n",p->mepc);
   return p;
 }
 
