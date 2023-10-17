a097eb37b0bc23973a9a381daaf75757352b69ce
tracer-ysyx <tracer@ysyx.org>
2023-10-17 14:52:18 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:52:18 up  5:27,  5 users,  load average: 0.67, 0.61, 0.63

--- nemu/src/trace/difftest.c
+++ nemu/src/trace/difftest.c
@@ -11,13 +11,14 @@ void init_difftest_for_npc() {
 void add_record() {
   char buf[200] = "";
   char temp[20];
+  sprintf(temp, "%x ",cpu.pc);
   for (int i = 0; i < 32; i++) {
     sprintf(temp, "%x ",cpu.gpr[i]);
 	strcat(buf, temp);
   }
   for (int i = 0; i < 6; i++) {
     unsigned int csr_reg = csr_read(i);
-	sprintf(temp, "%x",csr_reg);
+	sprintf(temp, "%x ",csr_reg);
   }
   fprintf(f, "%s\n",buf);
   fflush(f);
