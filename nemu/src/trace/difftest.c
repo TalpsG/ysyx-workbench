30f04f2b667ea3daba038de70add88ff2139ac7f
tracer-ysyx <tracer@ysyx.org>
2023-10-27 11:50:04 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:50:04 up  3:05,  6 users,  load average: 0.77, 0.51, 0.32

--- nemu/src/trace/difftest.c
+++ nemu/src/trace/difftest.c
@@ -3,9 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 extern  CPU_state cpu;
-static char difftest_file[] = "/home/talps/gitrepo/ysyx-workbench/nemu/build/difftest.txt";
+extern  char *img_file ;
+static char difftest_file[100] = "/home/talps/gitrepo/ysyx-workbench/nemu/build/";
 static FILE *f;
 void init_difftest_for_npc() {
+	char *filename = strrchr(img_file, '/');
+	strcat(difftest_file, filename);
+	printf("\n%s\n",difftest_file);
   f = fopen(difftest_file, "w+");
 }
 void add_record() {
