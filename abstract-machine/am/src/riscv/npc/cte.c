a20bf5153cddff717fa0379b94ca42410d655ef6
tracer-ysyx <tracer@ysyx.org>
2023-11-13 22:14:15 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:14:15 up 13:20,  5 users,  load average: 0.53, 0.54, 0.42

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -8,7 +8,11 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 1:; ev.event = EVENT_YIELD;break;
+    case 1: {
+      ev.event = EVENT_YIELD;
+      break;
+	  c->mepc += 4;
+	}
       default: ev.event = EVENT_ERROR; break;
     }
 
