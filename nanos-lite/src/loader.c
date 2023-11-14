54fa8e5229833c9be48f345bf7c69b783b0f41cb
tracer-ysyx <tracer@ysyx.org>
2023-11-14 15:32:49 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:32:49 up  6:51,  5 users,  load average: 0.63, 0.70, 0.68

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,16 +15,17 @@
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf32_Ehdr header_table ;
   ramdisk_read(&header_table, 0, sizeof(Elf32_Ehdr));
-  Elf32_Phdr *program_table = (void *)header_table.e_phoff;
   int phnum = header_table.e_phnum;
   //测试loader
   printf("\nready move\n");
   for (int i = 0; i < phnum; i++) {
-	if(program_table[i].p_type != PT_LOAD) continue;
-    size_t addr =  program_table[i].p_vaddr;
-	size_t p_offset = program_table[i].p_offset;
-	size_t p_filesz = program_table[i].p_filesz;
-	size_t p_memsz = program_table[i].p_memsz;
+    Elf32_Phdr program_table;
+    ramdisk_read(&program_table,header_table.e_phoff+i*sizeof(Elf32_Phdr),sizeof(Elf32_Phdr));
+	if(program_table.p_type != PT_LOAD) continue;
+    size_t addr =  program_table.p_vaddr;
+	size_t p_offset = program_table.p_offset;
+	size_t p_filesz = program_table.p_filesz;
+	size_t p_memsz = program_table.p_memsz;
   	printf("\nmove\n");
 	ramdisk_write((void *)p_offset, addr, p_filesz);
 	ramdisk_set(0, addr+p_filesz,p_memsz-p_filesz);
