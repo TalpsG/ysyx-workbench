33b264f5f1441e53a9f0be673489d217b3bd88d3
tracer-ysyx <tracer@ysyx.org>
2023-11-14 22:31:11 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:31:11 up 13:50,  4 users,  load average: 0.68, 0.47, 0.63

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,12 +22,10 @@ void hello_fun(void *arg) {
 void init_proc() {
   switch_boot_pcb();
 
-  Log("Initializing processes...");
+  //Log("Initializing processes...");
 
   // load program here
-  Log("uload...");
   naive_uload(NULL,NULL);
-  Log("uload end");
 
 }
 
