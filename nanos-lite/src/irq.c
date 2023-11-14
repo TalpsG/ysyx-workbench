419a84db2f44493bbbb12644db125350dab3eaca
tracer-ysyx <tracer@ysyx.org>
2023-11-14 19:31:00 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:31:00 up 10:50,  7 users,  load average: 0.62, 0.45, 0.28

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,6 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 	case EVENT_YIELD : {
-		printf("hello yield\n");
 		break;
 	}
     default: panic("Unhandled event ID = %d", e.event);
