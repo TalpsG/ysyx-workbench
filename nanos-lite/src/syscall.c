16864071362e9645612044d3d2df988ba87c5708
tracer-ysyx <tracer@ysyx.org>
2023-11-16 17:46:11 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:46:11 up  5:40,  6 users,  load average: 0.39, 0.37, 0.28

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,15 +4,22 @@ int sys_yield() {
   yield();
   return 0;
 }
+int sys_exit(int code) {
+  halt(code);
+}
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
   switch (a[0]) {
   case 1: {
 	sys_yield();
 	break;
   }
+  case 0: {
+    sys_exit(a[1]);
+	break;
+	}
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
