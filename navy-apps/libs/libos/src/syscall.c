423f648ac6256308de21d7457cdfcf83c5aba76d
tracer-ysyx <tracer@ysyx.org>
2023-11-17 14:12:23 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:12:23 up  5:08,  6 users,  load average: 0.42, 0.20, 0.11

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -66,7 +66,8 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-	return _syscall_(SYS_write, fd, buf, count);
+	uintptr_t ret = _syscall_(SYS_write, fd, buf, count);
+	return ret;
 }
 
 void *_sbrk(intptr_t increment) {
