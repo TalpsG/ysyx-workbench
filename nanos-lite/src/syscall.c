fee5bccf32def0fc05846b95eb48b491e113d46f
tracer-ysyx <tracer@ysyx.org>
2023-11-17 10:49:59 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:49:59 up  1:45,  5 users,  load average: 0.38, 0.35, 0.38

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -29,9 +29,9 @@ int sys_write(int fd, const void *buf, size_t n) {
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  a[1] = c->GPR2;
-  a[2] = c->GPR3;
-  a[3] = c->GPR4;
+  a[1] = c->gpr[10];
+  a[2] = c->gpr[11];
+  a[3] = c->gpr[12];
 
   switch (a[0]) {
   case 1: {
