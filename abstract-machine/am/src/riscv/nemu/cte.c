a8a6b8d33f98d6a073ec555450c1c577b2bd23ef
tracer-ysyx <tracer@ysyx.org>
2023-11-17 12:02:05 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:02:05 up  2:57,  6 users,  load average: 0.13, 0.23, 0.29

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -6,6 +6,7 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
+	printf("a7:%d",c->GPR1);
   if (user_handler) {
     Event ev = {0};
     switch (c->GPR1) {
