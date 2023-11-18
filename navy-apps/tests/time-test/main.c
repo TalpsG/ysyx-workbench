ec922a4d2cead26eef5860debbd8610ff9f54dbb
tracer-ysyx <tracer@ysyx.org>
2023-11-18 15:50:54 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:50:54 up  6:21,  6 users,  load average: 1.21, 0.86, 0.73

--- navy-apps/tests/time-test/main.c
+++ navy-apps/tests/time-test/main.c
@@ -7,9 +7,8 @@ int main() {
 	int times= 1;
     while (1) {
 		unsigned int now = NDL_GetTicks(); 
-		printf("now : %d\n",now);
 		if (now >= 500 * times) {
-			printf("times:%d",times++);
+			printf("times:%d\n",times++);
 		}
 	}
   return 0;
