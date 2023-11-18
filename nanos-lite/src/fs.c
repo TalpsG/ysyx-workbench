bf5c2f9d55a15b57e1263b183c4741415f0a9556
tracer-ysyx <tracer@ysyx.org>
2023-11-18 17:15:03 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:15:03 up  7:45,  6 users,  load average: 0.67, 0.55, 0.54

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -41,6 +41,9 @@ int fs_open(const char *pathname, int flags, int mode) {
 	return -1;
 }
 size_t fs_read(int fd, void *buf, size_t len) {
+  if (file_table[fd].read != NULL) {
+	return file_table[fd].read(buf,0,len);
+	}
 	ramdisk_read(buf, file_table[fd].disk_offset+file_table[fd].open_offset,len);
 	file_table[fd].open_offset += len;
 	return len;
