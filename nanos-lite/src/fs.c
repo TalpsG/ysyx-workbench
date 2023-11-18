c24bf9f70ecb34aac8408ec0053213108aa89ef0
tracer-ysyx <tracer@ysyx.org>
2023-11-18 10:00:16 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:00:16 up 30 min,  2 users,  load average: 0.49, 0.67, 0.57

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -16,7 +16,7 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, serial_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
 #include "files.h"
@@ -45,8 +45,8 @@ size_t fs_read(int fd, void *buf, size_t len) {
 	return len;
 }
 size_t fs_write(int fd, const void *buf, size_t len) {
-	if (fd == 1 || fd == 2) {
-		return serial_write(buf, 0, len)	;
+  if (file_table[fd].write != NULL) {
+    return file_table[fd].write(buf,0,len);
 	}
 	ramdisk_write(buf, file_table[fd].disk_offset +file_table[fd].open_offset, len);
 	file_table[fd].open_offset += len;
