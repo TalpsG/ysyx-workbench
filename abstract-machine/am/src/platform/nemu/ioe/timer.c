5c985fb6e5de366f59b1503ba53f5140e24c0889
tracer-ysyx <tracer@ysyx.org>
2023-11-18 13:02:28 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:02:28 up  3:32,  5 users,  load average: 0.55, 0.45, 0.24

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,9 +5,9 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-	//uint64_t temp2 = inw(RTC_ADDR+4);
+	uint64_t temp2 = inw(RTC_ADDR+4);
 	//uint64_t temp1 = inw(RTC_ADDR);
-	printf("%p\n",uptime->us);
+	printf("%p\n",temp2);
 	//char *p =(char *) &(uptime->us);
 	//for (int i = 0; i < 8; i++) {
 		//p[i] = inb(RTC_ADDR+i);
