6cef842231d8e24e3d4394c1ceb5ecfb6aa5d2a5
tracer-ysyx <tracer@ysyx.org>
2023-11-21 15:06:12 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:06:12 up  6:16,  6 users,  load average: 0.34, 0.37, 0.45

--- navy-apps/tests/pt-test/main.c
+++ navy-apps/tests/pt-test/main.c
@@ -3,7 +3,6 @@
 #include <sys/time.h>
 #include </home/talps/gitrepo/ysyx-workbench/navy-apps/libs/libfixedptc/include/fixedptc.h>
 int main() {
-	printf("1\n");
 	char *p = malloc(1);
 	assert(p);
   return 0;
