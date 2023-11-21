f67a79e52eb4f70189c6475fca33d857e0cd26cc
tracer-ysyx <tracer@ysyx.org>
2023-11-21 10:38:43 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:38:43 up  1:48,  3 users,  load average: 0.56, 0.39, 0.33

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -144,7 +144,7 @@ static void draw_text_row(char *s, int r) {
 static void display_menu(int n) {
   clear_display();
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
-  printf("logo_sf:%p screen:%p\n");
+  printf("logo_sf:%p screen:%p rect:%p\n",logo_sf,screen,&rect);
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
   printf("Available applications:\n");
   char buf[80];
