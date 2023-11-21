4582a2d2ebf980b28c35dca5908ba7d8fb72c6ef
tracer-ysyx <tracer@ysyx.org>
2023-11-21 14:10:26 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:10:26 up  5:20,  3 users,  load average: 0.45, 0.57, 0.60

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -31,7 +31,6 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 	}
 	for (int i = 0; i < h; i++) {
 		for (int j = 0; j < w; j++) {
-			printf("%p %p\n",(dst_y+i)*dst->w+dst_x+j+p_dst,p_src+(src_y+i)*src->w+src_x+j);
 			p_dst[(dst_y+i)*dst->w+dst_x+j] = p_src[(src_y+i)*src->w+src_x+j];
 		}
 
