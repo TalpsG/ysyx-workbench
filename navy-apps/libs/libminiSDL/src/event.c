c07557d739fb86d0c3ab404e9b5be7323adc90d9
tracer-ysyx <tracer@ysyx.org>
2023-11-21 18:02:33 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:02:33 up  9:12,  6 users,  load average: 0.82, 0.87, 0.77

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -16,12 +16,13 @@ int SDL_PushEvent(SDL_Event *ev) {
 
 int SDL_PollEvent(SDL_Event *ev) {
 	printf("poll\n");
-	char buf[20];
 	int ret = NDL_PollEvent(buf,20);
 	if(ret == 0) return 0;
 	printf("catch!\n");
 	int keydown, code;
 	printf("scanf\n");
+	char buf[200];
+	printf("buf:%p\n",buf);
 	sscanf(buf,"%d %d" ,keydown,code); 
 	printf("scan over\n");
 	ev->type = keydown?SDL_KEYDOWN:SDL_KEYUP;
