afb5d77744a0049528bd5cfeef0ab8b3e701be07
tracer-ysyx <tracer@ysyx.org>
2023-11-21 16:02:10 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:02:10 up  7:12,  6 users,  load average: 0.61, 0.40, 0.33

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -74,6 +74,7 @@ int main(int argc, char *argv[], char *envp[]) {
 
   screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);
   font = new BDF_Font(font_fname);
+  printf("w:%d,h:%d\n",font->w,font->h);
   logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
   assert(logo_sf);
   set_i_max();
--- navy-apps/libs/libbdf/src/SDL_bdf.cpp
+++ navy-apps/libs/libbdf/src/SDL_bdf.cpp
@@ -7,9 +7,8 @@ SDL_Surface* BDF_CreateSurface(BDF_Font *font, char ch, uint32_t fg, uint32_t bg
   if (!bm) return NULL;
   int w = font->w, h = font->h;
   printf("%d,%d\n",w ,h );
-  uint32_t *pixels = (uint32_t *)malloc(1);
+  uint32_t *pixels = (uint32_t *)malloc(w * h * sizeof(uint32_t));
   assert(pixels);
-  assert(0);
   for (int j = 0; j < h; j ++) {
     for (int i = 0; i < w; i ++) {
       pixels[j * w + i] = ((bm[j] >> i) & 1) ? fg : bg;
