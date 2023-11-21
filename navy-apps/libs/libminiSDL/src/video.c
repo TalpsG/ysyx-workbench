a093caf083d1ae2db4daf8eaf96a46733d8b37bf
tracer-ysyx <tracer@ysyx.org>
2023-11-21 10:45:21 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:45:21 up  1:55,  3 users,  load average: 0.21, 0.18, 0.24

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -44,7 +44,9 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 			p[i*w+j] = color;
 		}
 	}
+	printf("ndl draw\n");
 	NDL_DrawRect(dst->pixels,x,y,w,h);
+	printf("ndl draw over \n");
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
