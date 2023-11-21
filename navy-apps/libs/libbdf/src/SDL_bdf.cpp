a29a31646a7a991e356d38a52fde7c29c3d372be
tracer-ysyx <tracer@ysyx.org>
2023-11-21 15:51:58 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:51:58 up  7:02,  6 users,  load average: 0.51, 0.26, 0.27

--- navy-apps/libs/libbdf/src/SDL_bdf.cpp
+++ navy-apps/libs/libbdf/src/SDL_bdf.cpp
@@ -7,8 +7,9 @@ SDL_Surface* BDF_CreateSurface(BDF_Font *font, char ch, uint32_t fg, uint32_t bg
   if (!bm) return NULL;
   int w = font->w, h = font->h;
   printf("%d,%d\n",w ,h );
-  uint32_t *pixels = (uint32_t *)malloc(w * h * sizeof(uint32_t));
+  uint32_t *pixels = (uint32_t *)malloc(1);
   assert(pixels);
+  assert(0);
   for (int j = 0; j < h; j ++) {
     for (int i = 0; i < w; i ++) {
       pixels[j * w + i] = ((bm[j] >> i) & 1) ? fg : bg;
