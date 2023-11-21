bc59d3363992704dde4d801df9cd19bf008d21da
tracer-ysyx <tracer@ysyx.org>
2023-11-21 16:39:05 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:39:05 up  7:49,  6 users,  load average: 0.92, 0.81, 0.68

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -7,7 +7,6 @@
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
 	assert(dst && src);
 	assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
-	printf("%d %d\n",dst->w,dst->h);
 	uint32_t *p_src = src->pixels;
 	uint32_t *p_dst = dst->pixels;
 	int dst_x,dst_y,w,h,src_x,src_y;
