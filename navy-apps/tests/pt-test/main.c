cc402f844656f5f207c3c0ccb8a568a5f240a66e
tracer-ysyx <tracer@ysyx.org>
2023-11-21 14:57:41 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:57:41 up  6:07,  6 users,  load average: 0.79, 0.54, 0.51

--- navy-apps/tests/pt-test/main.c
+++ navy-apps/tests/pt-test/main.c
@@ -3,39 +3,8 @@
 #include <sys/time.h>
 #include </home/talps/gitrepo/ysyx-workbench/navy-apps/libs/libfixedptc/include/fixedptc.h>
 int main() {
-	fixedpt A = 1<<8;
-	fixedpt B = fixedpt_floor(A);
-	assert(B == (1<<8));
-	B = fixedpt_ceil(A);
-	assert(B == A);
-	fixedpt C = A+1;
-	B = fixedpt_floor(C);
-	assert(B == (1<<8));
-	B = fixedpt_ceil(C);
-	assert(B == (2<<8));
-	C = A-1;
-	B = fixedpt_floor(C);
-	assert(B == 0);
-	B = fixedpt_ceil(C);
-	assert(B == A);
-	fixedpt A_neg = (~A)+1;
-	fixedpt B_neg = fixedpt_floor(A_neg);
-	fixedpt C_neg ;
-	assert(B_neg == A_neg);
-	B_neg = fixedpt_ceil(A_neg);
-	assert(B_neg == A_neg);
-	B_neg = A_neg +1;
-	C_neg = fixedpt_floor(B_neg);
-	assert(C_neg == A_neg);
-	B_neg = fixedpt_ceil(B_neg);
-	assert(B_neg == 0);
-	B_neg = A_neg -1;
-	C_neg = fixedpt_floor(B_neg);
-	printf("%p %p\n",A_neg,C_neg);
-	assert(C_neg == ((~(2<<8))+1));
-	B_neg = fixedpt_ceil(B_neg);
-	assert(B_neg == A_neg);
-
-
+	char *p = malloc(1);
+	printf("%p\n",p);
+	assert(p);
   return 0;
 }
