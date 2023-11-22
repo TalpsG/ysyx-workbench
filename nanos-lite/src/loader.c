a3347f57cdd35e9ccbf83b7bc092f9f5d113eafb
tracer-ysyx <tracer@ysyx.org>
2023-11-22 14:55:17 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:17 up  5:54,  3 users,  load average: 0.27, 0.31, 0.30

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -29,8 +29,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 	size_t p_memsz= program_table.p_memsz;
 
 	ramdisk_read((void*)addr,file_offset+ p_offset,p_filesz);
-	printf("%p %d\n",addr+file_offset+p_filesz,p_memsz - p_filesz);
-	memset((void*)(addr+file_offset+ p_filesz),0,p_memsz - p_filesz);
+	printf("%p %d\n",addr+p_filesz,p_memsz - p_filesz);
+	memset((void*)(addr+ p_filesz),0,p_memsz - p_filesz);
   }
   fs_close(fd);
   return (uintptr_t)header_table.e_entry;
