933e370f03d702f6ba8ba2382060f554a679a019
tracer-ysyx <tracer@ysyx.org>
2023-11-24 02:13:48 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:13:48 up  2:59,  4 users,  load average: 0.34, 0.51, 0.56

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -82,7 +82,7 @@ size_t fs_write(int fd, const void *buf, size_t len) {
 	Finfo *p = &file_table[fd];
 	size_t  real_len;
 	if (p->write != NULL) {
-		if(fd!=5) printf("write :%d \n",fd);
+		serial_write("10\n", 0, 3);
 		real_len = p->write(buf,p->open_offset,len);
 		p->open_offset += real_len;
 		return real_len;
