01e3774054d13973670e9d52603459f6c4e72620
tracer-ysyx <tracer@ysyx.org>
2023-11-23 15:15:08 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:15:08 up  6:09,  3 users,  load average: 0.94, 0.94, 0.86

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 	case EVENT_YIELD: {
-		printf("yield");
+		c->mepc += 4;
 		yield();
 		break;
 	}
