07843bd6659a12496b08176a7115fce1e794624e
tracer-ysyx <tracer@ysyx.org>
2023-11-24 01:53:56 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:53:56 up  2:39,  4 users,  load average: 0.63, 0.58, 0.57

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 #include <NDL.h>
 int main() {
-	struct timeval t;
-	NDL_Init(0);
-	gettimeofday(&t, NULL);
-	uint32_t time = NDL_GetTicks();
-	int i= 1;
-	while (1) {
-		if (NDL_GetTicks() - time > 500) {
-			time = NDL_GetTicks();
-			printf("%d half\n",i++);
-		}
-	}
+	printf("hello\n");
   return 0;
 }
