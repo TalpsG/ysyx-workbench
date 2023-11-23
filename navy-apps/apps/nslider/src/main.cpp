ea2f506f6ba5e821bc4b299f9a054dbe9a128991
tracer-ysyx <tracer@ysyx.org>
2023-11-24 01:02:02 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:02:02 up  1:47,  4 users,  load average: 1.27, 0.75, 0.53

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -25,7 +25,9 @@ void render() {
   }
   char fname[256];
   sprintf(fname, path, cur);
+  printf("load bmp\n");
   slide = SDL_LoadBMP(fname);
+  printf("load over\n");
   assert(slide);
   SDL_UpdateRect(slide, 0, 0, 0, 0);
 }
