2f9d7056e4d080c99df8808ed0619450d436dfea
tracer-ysyx <tracer@ysyx.org>
2023-11-23 20:05:01 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:05:01 up 10:59,  5 users,  load average: 0.69, 0.47, 0.58

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -4,7 +4,6 @@ int main() {
 	struct timeval t;
 	gettimeofday(&t, NULL);
 	uint64_t time = t.tv_usec;
-	uint32_t *p = &t.tv_usec;
 	while (1) {
 		gettimeofday(&t, NULL);
 		if (t.tv_usec - time > 500000 ) {
