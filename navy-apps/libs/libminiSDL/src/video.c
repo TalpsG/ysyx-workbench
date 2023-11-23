6c6191ab2d56ebccc09d24c1f50858ab301ba942
tracer-ysyx <tracer@ysyx.org>
2023-11-24 02:48:55 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:48:55 up  3:34,  4 users,  load average: 1.29, 0.93, 0.77

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -5,45 +5,45 @@
 #include <stdlib.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
-  assert(dst && src);
-  assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
-  int s_x,s_y,d_x,d_y,w,h;
+  //assert(dst && src);
+  //assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
+  //int s_x,s_y,d_x,d_y,w,h;
 
-  if (srcrect == NULL) {
-	w = src->w;
-	h = src->h;
-	s_x = 0;
-	s_y = 0;
-  } else {
-	w = srcrect->w;
-	h = srcrect->h;
-	s_x = srcrect->x;
-	s_y = srcrect->y;
-  }
-  if (dstrect == NULL) {
-	d_x = 0;d_y=0;
-  } else {
-	d_x = dstrect->x;
-	d_y = dstrect->y;
-  }
-  if (dst->format->BitsPerPixel == 32) {
-	uint32_t *dst_pixels = dst->pixels;
-	uint32_t *src_pixels = src->pixels;
-    for (int i = 0; i < h; i++) {
-      for (int j = 0; j < w; j++) {
-		dst_pixels[(d_y+i)*dst->w+d_x+j] = src_pixels[(s_y+i)*src->w+s_x+j];
-	  }
-	}
-  } else if (dst->format->BitsPerPixel == 8) {
-	uint8_t *dst_pixels = dst->pixels;
-	uint8_t *src_pixels = src->pixels;
-    for (int i = 0; i < h; i++) {
-      for (int j = 0; j < w; j++) {
-		dst_pixels[(d_y+i)*dst->w+d_x+j] = src_pixels[(s_y+i)*src->w+s_x+j];
-	  }
-	}
+  //if (srcrect == NULL) {
+	//w = src->w;
+	//h = src->h;
+	//s_x = 0;
+	//s_y = 0;
+  //} else {
+	//w = srcrect->w;
+	//h = srcrect->h;
+	//s_x = srcrect->x;
+	//s_y = srcrect->y;
+  //}
+  //if (dstrect == NULL) {
+	//d_x = 0;d_y=0;
+  //} else {
+	//d_x = dstrect->x;
+	//d_y = dstrect->y;
+  //}
+  //if (dst->format->BitsPerPixel == 32) {
+	//uint32_t *dst_pixels = dst->pixels;
+	//uint32_t *src_pixels = src->pixels;
+    //for (int i = 0; i < h; i++) {
+      //for (int j = 0; j < w; j++) {
+		//dst_pixels[(d_y+i)*dst->w+d_x+j] = src_pixels[(s_y+i)*src->w+s_x+j];
+	  //}
+	//}
+  //} else if (dst->format->BitsPerPixel == 8) {
+	//uint8_t *dst_pixels = dst->pixels;
+	//uint8_t *src_pixels = src->pixels;
+    //for (int i = 0; i < h; i++) {
+      //for (int j = 0; j < w; j++) {
+		//dst_pixels[(d_y+i)*dst->w+d_x+j] = src_pixels[(s_y+i)*src->w+s_x+j];
+	  //}
+	//}
 
-  }
+  //}
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
