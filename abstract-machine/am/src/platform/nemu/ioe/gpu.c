8ccbe2fa4885cabe25dbc24b9d6bbfb3ca609557
tracer-ysyx <tracer@ysyx.org>
2023-11-28 09:23:45 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:23:45 up  1:20,  3 users,  load average: 0.65, 0.44, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,10 +2,7 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
-#define SYNC_ADDR (VGACTL_ADDR + 4)
-#define SIZE_ADDR (VGACTL_ADDR)
 
-#define min(x,y) ( (x < y) ? x : y )
 void __am_gpu_init() {
 }
 
@@ -19,30 +16,16 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-	//uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-	//signed int vga_w = inl(VGACTL_ADDR)>>16;
-	//int w = ctl->w,h = ctl->h;
-	//int x = ctl->x,y=ctl->y;
-	//int pos = (y)*vga_w+x;
-	//for (int i = 0; i < h; i++) {
-          //for (int j = 0; j < w; j++) {
-            //fb[pos+i*vga_w+j] = ((uint32_t*)ctl->pixels)[i*w+j];
-		//}
-	//}
-	if(ctl->w != 0 && ctl->h != 0){
-	    int W = inw(SIZE_ADDR+2);  
-		int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-		uint32_t* p = ctl->pixels;
-
-		uint32_t p_pos = 0;
-		uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-		uint32_t cp_byte = sizeof(uint32_t) * min(w , W - x);
-		for(int row = y; row < y + h; ++ row){
-			memcpy(fb + x + row * W, p + (p_pos), cp_byte);
-			// printf("write pixels[%d] of %d to fb[%d, %d]\n", p_pos, p[p_pos], row , x);
-			p_pos += w;
+	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+	signed int vga_w = inl(VGACTL_ADDR)>>16;
+	int w = ctl->w,h = ctl->h;
+	int x = ctl->x,y=ctl->y;
+	int pos = (y)*vga_w+x;
+	for (int i = 0; i < h; i++) {
+          for (int j = 0; j < w; j++) {
+            fb[pos+i*vga_w+j] = ((uint32_t*)ctl->pixels)[i*w+j];
 		}
-  }
+	}
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
