1e8540717e578503d12d1cf820ff1c1459c41cc2
tracer-ysyx <tracer@ysyx.org>
2023-11-29 10:32:04 +0800

    > compile NEMU ysyx_22040000 王伟涛 Linux talps 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:32:04 up  1:39,  3 users,  load average: 0.44, 0.50, 0.62

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -17,12 +17,11 @@ int main(int argc, char *argv[]) {
   // setup display
   int win_w = font->w * W;
   int win_h = font->h * H;
-  printf("%d %d\n",win_w,win_h);
   screen = SDL_SetVideoMode(win_w, win_h, 32, SDL_HWSURFACE);
 
   term = new Terminal(W, H);
 
-  if (argc < 2) { builtin_sh_run(); }
+  if (argc < 2) { printf("builtin\n");builtin_sh_run(); }
   else { extern_app_run(argv[1]); }
 
   // should not reach here
