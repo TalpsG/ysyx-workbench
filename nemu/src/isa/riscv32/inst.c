62db8332ed04f538937f4dce63a0c6863504889c
wwt <19818962332@163.com>
2023-12-03 11:22:58 +0800

    2

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -34,7 +34,28 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immJ() do { *imm = SEXT((BITS(i,20,20)<<19) + (BITS(i, 10, 1)<< 9) + (BITS(i, 11, 11) << 8) + BITS(i,19,12),20); } while(0)
+
+
+#define immJ()                        \
+  do                                  \
+  {                                   \
+    uint32_t sign= BITS(i, 31, 31)<<20;   \
+    uint32_t a = BITS(i, 19, 12)<<12;     \
+    uint32_t b = BITS(i, 20, 20)<<11;     \
+    uint32_t c = BITS(i, 30, 21)<<1;     \
+    *imm = SEXT(sign|a|b|c,21); \
+  } while (0)
+
+#define immB()                                               \
+  do                                                         \
+  {                                                          \
+    uint32_t a= BITS(i, 31, 31)<<12;  \
+    uint32_t b= BITS(i, 7 , 7)<<11;  \
+    uint32_t c= BITS(i, 30 , 25)<<5;  \
+    uint32_t d= BITS(i, 11 , 8)<<1;  \
+    *imm = SEXT(a|b|c|d, 13); \
+  } while (0)
+
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
