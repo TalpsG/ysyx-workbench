e7bdf4de3cfd2bcb078e7c3cde479b4cb224ac0d
wwt <19818962332@163.com>
2023-12-04 12:05:24 +0800

    printf comp

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,7 +6,86 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+    va_list args;
+    va_start(args, fmt);
+    int written = 0;
+
+    while (*fmt) {
+        if (*fmt == '%') {
+            fmt++;
+            if (*fmt == 's') {
+                const char *s = va_arg(args, const char *);
+                while (*s) {
+                    putch(*s);
+                    s++;
+                    written++;
+                }
+            } else if (*fmt == 'd') {
+                int d = va_arg(args, int);
+                int length = 0;
+                int temp = d;
+                if (d < 0) {
+                    putch('-');
+                    written++;
+                    temp = -d;
+                }
+                while (temp != 0) {
+                    temp /= 10;
+                    length++;
+                }
+                if (d == 0) {
+                    putch('0');
+                    written++;
+                } else {
+                    temp = d;
+                    char buffer[length];
+                    for (int i = length - 1; i >= 0; i--) {
+                        buffer[i] = (temp % 10) + '0';
+                        temp /= 10;
+                    }
+                    for (int i = 0; i < length; i++) {
+                        putch(buffer[i]);
+                        written++;
+                    }
+                }
+            } else if (*fmt == 'x') {
+                unsigned int x = va_arg(args, unsigned int);
+                int length = 0;
+                unsigned int temp = x;
+                while (temp != 0) {
+                    temp /= 16;
+                    length++;
+                }
+                if (x == 0) {
+                    putch('0');
+                    written++;
+                } else {
+                    temp = x;
+                    char buffer[length];
+                    for (int i = length - 1; i >= 0; i--) {
+                        int digit = temp % 16;
+                        if (digit < 10) {
+                            buffer[i] = digit + '0';
+                        } else {
+                            buffer[i] = digit - 10 + 'a';
+                        }
+                        temp /= 16;
+                    }
+                    for (int i = 0; i < length; i++) {
+                        putch(buffer[i]);
+                        written++;
+                    }
+                }
+            }
+        } else {
+            putch(*fmt);
+            written++;
+        }
+        fmt++;
+    }
+    va_end(args);
+    return written;
+
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
